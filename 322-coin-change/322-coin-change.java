6051d6fbd3d9571f53a978b814f451f3db8e9baf
piyusheklavya777 <70015666+piyusheklavya777@users.noreply.github.com>
2022-01-16 11:00:35 +0530

    Time: 13 ms (73.01%), Space: 38.2 MB (83.09%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -3,23 +3,19 @@ class Solution {
         
         int MAX = 1_000_000;
         
-        Arrays.sort(coins);
-        
         int clen = coins.length;
         
         int[] dp = new int[n + 1];
         
-        Arrays.fill(dp, MAX);
-        
-        dp[0] = 0; // coins needed to make total 0.
-        
         for (int toMake = 1; toMake <= n; toMake++) {
+            
+            dp[toMake] = MAX;
             
             for (int ci = clen - 1; ci >= 0; ci--) {
                 
                 int coinValue = coins[ci];
                 
-                if (toMake < coinValue || dp[toMake - coinValue] == MAX) continue; // need smaller coin
+                if (toMake < coinValue || dp[toMake - coinValue] == MAX) continue;
                 
                 dp[toMake] = Math.min(dp[toMake], 1 + dp[toMake - coinValue]);
         
