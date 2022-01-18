44b20a0cbeed80c41967820055ac79171d76a682
piyusheklavya777 <70015666+piyusheklavya777@users.noreply.github.com>
2022-01-18 22:13:28 +0530

    Time: 2 ms (31.85%), Space: 49.2 MB (24.30%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] bed, int toPlant) {
+        
+        Set<Integer> plantedAt = new HashSet<>();
+        
+        int n = bed.length, planted = 1, notPlanted = 0;
+        
+        for (int i = 0; i < n; i++) {
+            
+            if (toPlant == 0) break;
+            
+            if (bed[i] == planted) continue;
+            
+            if (
+                (i == 0 || bed[i-1] == notPlanted) && 
+                (i == n-1 || bed[i+1] == notPlanted)
+            ) {
+                
+                bed[i] = 1;
+                toPlant--;
+            }
+            
+        }
+        
+        return toPlant == 0;
+        
+    }
+}
