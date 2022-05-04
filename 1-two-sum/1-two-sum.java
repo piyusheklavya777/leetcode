8ce12b0ca04d75992cba5cee1fef57361c6bb191
Piyush Eklavya <70015666+piyusheklavya777@users.noreply.github.com>
2022-05-04 23:53:45 +0530

    Time: 3 ms (85.40%), Space: 45.6 MB (34.74%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,20 +1,36 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] a, int t) {
         
-        Map<Integer, Integer> location = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         
-        Integer suppliment;
+        for (int i = 0; i < a.length; i++) {
+            
+            map.put(a[i], i);
+            
+            int compliment = t - a[i];
+            
+            int complimentIndex = map.getOrDefault(compliment, -1);
+            
+            if (complimentIndex == -1 || complimentIndex == i) continue;
+            
+            return new int[]{complimentIndex, i};
+            
+        }
         
-        for (int i = 0; i < nums.length; i++)
-            location.put(nums[i], i);
-        
-        for (int i = 0; i < nums.length; i++) {
-            suppliment = location.getOrDefault(target-nums[i], Integer.MAX_VALUE);
-            if (suppliment == Integer.MAX_VALUE || suppliment == i)
+        for (int i = 0; i < a.length; i++) {
+            
+            int compliment = t - a[i];
+            
+            int complimentIndex = map.getOrDefault(t - a[i], -1);
+            
+            if (complimentIndex == -1 || complimentIndex == i)
                 continue;
-            return new int[] {i, suppliment};
+            
+            return new int[]{i, complimentIndex};
+            
         }
         
         throw new IllegalArgumentException();
+        
     }
 }
